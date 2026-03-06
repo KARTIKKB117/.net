ffcb8328eabeb4dcf0da57a3b1123e1a6578b8cc
Kartik  K Bandekar <bandekar.kartik117@gmail.com>
2026-03-06 12:21:59 +0530

    Add files via upload

---
+++ Rolebased/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rolebased.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
